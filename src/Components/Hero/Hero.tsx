8e117dfc99ab4ab88f98ec55302c3192d911c8f0
Heaven <gabrielheavenlydawn@gmail.com>
2024-02-19 20:16:22 +0100

    Hero Section

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div id="hero">
+      <div className="text-[#F6F6F6] py-[100px] w-[1000px] mx-auto">
+        <p className="text-3xl font-semibold my-5">Food app</p>
+        <h1 className="text-7xl font-bold my-5 leading-[70px]">
+          Why stay hungry when you can order form Bella Onojie
+        </h1>
+        <p className="text-3xl my-5">
+          Download the bella onoje’s food app now on
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
