efbcec68da7d4b5d2d88e39b43ef9664933754f7
Heaven <gabrielheavenlydawn@gmail.com>
2024-02-19 19:49:27 +0100

    Header section

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Logo from "../../../public/Images/Logo.png";
 const Header = () => {
   return (
-    <div>
+    <div className="flex justify-around items-center">
       <div>
         <Image src={Logo} alt="Logo" />
       </div>
