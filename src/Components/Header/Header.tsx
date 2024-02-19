5451d4909cdff91ddbdfec626bcc4e990b252dd8
Heaven <gabrielheavenlydawn@gmail.com>
2024-02-19 19:33:05 +0100

    Header Section

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import Image from "next/image";
+import Logo from "../../../public/Images/Logo.png";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div>
+      <div>
+        <Image src={Logo} alt="Logo" />
+      </div>
 
-export default Header
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Product</li>
+          <li>Faq</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
