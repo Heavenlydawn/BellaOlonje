895d85b9f990718c0705ed8c3fc2a1c560281edd
Heaven <gabrielheavenlydawn@gmail.com>
2024-02-29 15:03:02 +0100

    Nav bar Functionality

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Logo from "../../../public/Images/Logo.png";
 
 const Header: React.FC = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(true);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
     setMobileMenuOpen(!mobileMenuOpen);
@@ -52,11 +52,6 @@ const Header: React.FC = () => {
               )}
             </svg>
           </div>
-           {/* <div className="md:hidden cursor-pointer">
-            <button onClick={toggleMobileMenu} className="text-3xl px-4">
-              ☰
-            </button>
-          </div> */}
         </nav>
 
         {/* Mobile Menu */}
